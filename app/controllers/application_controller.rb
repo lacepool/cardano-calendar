8345f2bb82024359dc629343d6f29126df423b2d
Robin Böning <robin.boening@gmail.com>
2024-03-20 14:40:43 +0100

    store time zone in session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,8 @@ class ApplicationController < ActionController::Base
   private
 
   def set_time_zone(&block)
-    Time.use_zone(params.fetch(:tz, "Etc/UTC"), &block)
+    session[:tz] = params[:tz] if params[:tz]
+    Time.use_zone(session[:tz] || "Etc/UTC", &block)
 
   rescue ArgumentError => e
     flash[:warning] = "#{e} – Falling back to UTC"
