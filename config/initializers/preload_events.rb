48557e9d120d364a2ea8e409e0c72d9a52e39991
Robin Böning <robin.boening@gmail.com>
2024-03-22 22:20:38 +0100

    don't preload events when compiling assets

--- config/initializers/preload_events.rb
+++ config/initializers/preload_events.rb
@@ -1,5 +1,7 @@
-events = "#{Rails.root}/app/models/events"
+unless ENV["SECRET_KEY_BASE_DUMMY"]
+  events = "#{Rails.root}/app/models/events"
 
-Rails.application.config.to_prepare do
-  Rails.autoloaders.main.eager_load_dir(events)
+  Rails.application.config.to_prepare do
+    Rails.autoloaders.main.eager_load_dir(events)
+  end
 end
