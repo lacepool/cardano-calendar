c1ef0bd12573be7c471d5efee1548706c42dc4cf
Robin Böning <robin.boening@gmail.com>
2024-03-22 12:40:46 +0100

    add health check, taken from rails 7.1
    can be removed after upgrading to rails 7.1

---
+++ app/controllers/health_controller.rb
@@ -0,0 +1,20 @@
+class HealthController < ActionController::Base
+  rescue_from(Exception) { render_down }
+
+  def show
+    render_up
+  end
+
+  private
+    def render_up
+      render html: html_status(color: "green")
+    end
+
+    def render_down
+      render html: html_status(color: "red"), status: 503
+    end
+
+    def html_status(color:)
+      %(<!DOCTYPE html><html><body style="background-color: #{color}"></body></html>).html_safe
+    end
+end
